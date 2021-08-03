b5ffc1a22c9dc229ae607b874cfcfc6f9d28240f
janchua19 <jan.edward.chua@oocl.com>
2021-08-03 17:11:07 +0800

    [MultiplicationTable][JanEdwardChua] feat: Added functions

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -4,4 +4,47 @@ public class MultiplicationTable {
     public String create(int startNumber, int endNumber) {
         return null;
     }
+
+    public boolean isValid(int startNumber, int endNumber)
+    {
+        return isValid;
+    }
+    boolean isValid = false;
+
+    public boolean isNumberInRange (int number)
+    {
+        return (1 >= number) && (number >= 1000 );
+    }
+
+    public boolean isEndBiggerThanStart (int startNumber, int endNumber)
+    {
+        return startNumber < endNumber || startNumber == endNumber;
+    }
+
+    public String generateMultiplicationExpression (int startNumber, int endNumber)
+    {
+        int product = 0;
+        product = startNumber * endNumber;
+        String multiplicationExpression = String.valueOf(product);
+        return multiplicationExpression;
+    }
+
+    public String generateMultiplicationLine (int startNumber, int row)
+    {
+        int product = 0;
+        product = startNumber * row;
+        String multiplicationLine = String.valueOf(product);
+        System.out.print(multiplicationLine);
+        return multiplicationLine;
+    }
+
+    public String generateMultiplicationTable (int startNumber, int endNumber)
+    {
+        int product = 0;
+        product = startNumber * endNumber;
+        String multiplicationTable = String.valueOf(product);
+        System.out.println(multiplicationTable);
+        return multiplicationTable;
+    }
 }
+
